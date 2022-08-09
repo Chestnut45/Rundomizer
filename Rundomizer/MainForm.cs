6abd4c85141cffdbf562e0cf196c504a2a8c42b4
Chestnut45 <cpt20xx@gmail.com>
2022-08-09 13:50:42 -0300

    Added all orb icons

--- Rundomizer/MainForm.cs
+++ Rundomizer/MainForm.cs
@@ -66,7 +66,40 @@ namespace Rundomizer
             OrbListView.SmallImageList.ImageSize = new Size(60, 60);
             OrbListView.SmallImageList.ColorDepth = ColorDepth.Depth24Bit;
 
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Stone.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Daggorb.png"));
             OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Sworb.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Sphear.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Bramball.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Rubborb.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Splatorb.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Orbsium.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Bouldorb.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Orbelisk.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Swoltorb.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Omegorb.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Matryorbshka.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Reorbanizer.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Allorbnothing.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Infernorb.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Icircle.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Ball Lightning.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Darkness Eterball.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Poltorbgeist.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Echorb.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Refreshorb.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Critiball.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Ohmygorb!.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Extra-Orbinary.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Bob-Orb.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Concentrication.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Doctorb.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Nosforbatu.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Necorbmancer.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Morbidorb.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Memorb.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Jack-Orb-Lantern.png"));
+            OrbListView.SmallImageList.Images.Add(Image.FromFile("Orbs/Etherwheel.png"));
         }
 
         private async void GenerateRunButton_Click(object sender, EventArgs e)
